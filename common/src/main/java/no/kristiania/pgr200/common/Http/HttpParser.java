043bb7bc1a85b4475d82ff1e821ed05862aa57cd
Andreas Storesund Madsen <andreas@asmadsen.no>
2018-09-11 13:05:24 +0200

    ✨ Finished HttpRequest and HttpResponse with tests
      resolves #1

--- common/src/main/java/no/kristiania/pgr200/common/Http/HttpParser.java
+++ common/src/main/java/no/kristiania/pgr200/common/Http/HttpParser.java
@@ -2,15 +2,19 @@ package no.kristiania.pgr200.common.Http;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class HttpParser {
 
     public HttpRequest parseRequest(InputStream stream) throws IOException {
+        InputStreamReader reader = new InputStreamReader(stream, UTF_8);
         HttpRequest request = new HttpRequest();
 
-        String line = readNextLine(stream);
+        String line = readNextLine(reader);
         if (!line.trim().isEmpty()) {
             String[] parts = line.trim().split(" ", 3);
             request.setHttpMethod(HttpMethod.valueOf(parts[0]));
@@ -18,53 +22,54 @@ public class HttpParser {
             request.setHttpVersion(parts[2]);
         }
 
-        request.setHeaders(parseHeaders(stream));
+        request.setHeaders(parseHeaders(reader));
 
-        request.setBody(parseBody(stream));
+        request.setBody(parseBody(reader));
 
         return request;
     }
 
     public HttpResponse parseResponse(InputStream stream) throws IOException {
+        InputStreamReader reader = new InputStreamReader(stream, UTF_8);
         HttpResponse response = new HttpResponse();
 
-        String line = readNextLine(stream);
+        String line = readNextLine(reader);
         if (!line.trim().isEmpty()) {
             String[] parts = line.trim().split(" ", 3);
             response.setHttpVersion(parts[0]);
             response.setStatus(HttpStatus.valueOf(Integer.parseInt(parts[1])));
         }
 
-        response.setHeaders(parseHeaders(stream));
+        response.setHeaders(parseHeaders(reader));
 
-        response.setBody(parseBody(stream));
+        response.setBody(parseBody(reader));
 
         return response;
     }
 
-    private String parseBody(InputStream stream) throws IOException {
+    private String parseBody(InputStreamReader reader) throws IOException {
         StringBuilder builder = new StringBuilder();
         int c;
-        while ((c = stream.read()) != -1) {
+        while ((c = reader.read()) != -1) {
             builder.append((char) c);
         }
         return builder.toString();
     }
 
-    private Map<String, String> parseHeaders(InputStream stream) throws IOException {
+    private Map<String, String> parseHeaders(InputStreamReader reader) throws IOException {
         Map<String, String> headers = new HashMap<>();
         String line;
-        while (!(line = readNextLine(stream)).trim().isEmpty()) {
+        while (!(line = readNextLine(reader)).trim().isEmpty()) {
             String[] parts = line.trim().split(": ");
             headers.put(HttpUtils.capitalizeHeaderKey(parts[0].trim()), parts[1].trim());
         }
         return headers;
     }
 
-    private String readNextLine(InputStream stream) throws IOException {
+    private String readNextLine(InputStreamReader reader) throws IOException {
         StringBuilder builder = new StringBuilder();
         int c;
-        while ((c = stream.read()) != -1) {
+        while ((c = reader.read()) != -1) {
             builder.append((char) c);
             if (c == '\n') break;
         }
--- common/src/main/java/no/kristiania/pgr200/common/Http/HttpResponse.java
+++ common/src/main/java/no/kristiania/pgr200/common/Http/HttpResponse.java
@@ -1,8 +1,44 @@
 package no.kristiania.pgr200.common.Http;
 
+import com.google.gson.JsonElement;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
 public class HttpResponse extends HttpCommon {
     private HttpStatus status;
-    private String httpVersion;
+
+    public HttpResponse(HttpStatus status, String body) {
+        this(status, null, body);
+    }
+
+    public HttpResponse(HttpStatus status, Map<String, String> headers, String body) {
+        super(headers, body);
+        this.status = status;
+    }
+
+    public HttpResponse() {
+        super();
+    }
+
+    public HttpResponse(HttpStatus status) {
+        this(status, null, "");
+    }
+
+    public HttpResponse(HttpStatus status, HashMap<String, String> headers) {
+        this(status, headers, "");
+    }
+
+    public HttpResponse(HttpStatus status, JsonElement body) {
+        this(status, null, body);
+    }
+
+    public HttpResponse(HttpStatus status, Map<String, String> headers, JsonElement body) {
+        super(headers, body);
+        this.status = status;
+    }
 
     public HttpStatus getStatus() {
         return status;
@@ -12,16 +48,8 @@ public class HttpResponse extends HttpCommon {
         this.status = status;
     }
 
-    public String getHttpVersion() {
-        return httpVersion;
-    }
-
-    public void setHttpVersion(String httpVersion) {
-        this.httpVersion = httpVersion;
-    }
-
     @Override
     protected byte[] getFirstLine() {
-        return new byte[0];
+        return String.format("%s %s %s\r\n", this.httpVersion, this.status.getStatusCode(), this.status.realName()).getBytes(UTF_8);
     }
 }
