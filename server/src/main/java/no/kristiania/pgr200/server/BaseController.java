1b8646d6cb6404c56c034161ba265eee06c38f20
alphonsesantoro <kslagnes@mimer.no>
2018-09-11 11:18:09 +0200

    #2 Test

--- server/src/main/java/no/kristiania/pgr200/server/BaseController.java
+++ server/src/main/java/no/kristiania/pgr200/server/BaseController.java
@@ -1,39 +1,59 @@
 package no.kristiania.pgr200.server;
 
+import no.kristiania.pgr200.common.Http.HttpMethod;
+
 import java.io.PrintWriter;
 import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class BaseController extends HeaderResponse {
+public class BaseController extends HeaderResponse implements Routing {
 
     private PrintWriter out;
     private String id;
     private String parameters;
+    private String route;
 
     public Method getMethodFromAnnotation(String request) {
         String[] tokens = request.split(" ");
-        final List<Method> controllerMethods = new ArrayList<>(Arrays.asList(TalksController.class.getDeclaredMethods()));
-        Method requestedMethod = null;
-        for (Method controllerMethod : controllerMethods) {
-            ApiRequest annotation = controllerMethod.getAnnotation(ApiRequest.class);
-            if (annotation != null) {
-                String route = tokens[1].split("/")[2];
-                if (route.contains("?")) {
-                    setParameters(route.split("[?]")[1]);
-                    route = route.split("[?]")[0];
-                }
-                Pattern pattern = Pattern.compile(annotation.route());
-                Matcher matcher = pattern.matcher(route);
-                if (tokens[0].matches(annotation.action()) && matcher.find()) {
-                    requestedMethod = controllerMethod;
+        setRoute(tokens[1]);
+        final Method[] requestedMethod = {null};
+        getControllers().forEach((controllerClass,apiAnnotation) -> {
+            Pattern regex = Pattern.compile(apiAnnotation.value());
+            Matcher matcher = regex.matcher(getRoute());
+            if(matcher.find()){
+                try {
+                    requestedMethod[0] = controllerClass.parseRoute(HttpMethod.valueOf(tokens[0]), getRoute());
+                } catch (Exception e) {
+                    // TODO: Respond with HttpCode
+
                 }
             }
-        }
-        return requestedMethod;
+        });
+
+//        if (route.contains("?")) {
+//            setParameters(route.split("[?]")[1]);
+//            route = route.split("[?]")[0];
+//        }
+//        String controllerName = route.split("/")[2];
+//        final List<Method> controllerMethods = new ArrayList<>(
+//                Arrays.asList(Class.forName(getClass().getPackage().getName() + "." +
+//                        Helpers.capitalize(controllerName) + "Controller").getDeclaredMethods()));
+
+//        for (Method controllerMethod : controllerMethods) {
+//            ApiRequest annotation = controllerMethod.getAnnotation(ApiRequest.class);
+//            if (annotation != null) {
+//                Pattern pattern = Pattern.compile(annotation.route());
+//                Matcher matcher = pattern.matcher(route);
+//                System.out.println(tokens[0].matches(annotation.action().name()) && matcher.find());
+//                if (tokens[0].matches(annotation.action().name()) && matcher.find()) {
+//                    setRoute(route);
+//                    requestedMethod = controllerMethod;
+//                }
+//            }
+//        }
+        return requestedMethod[0];
     }
 
     public PrintWriter getOut() {
@@ -58,5 +78,26 @@ public class BaseController extends HeaderResponse {
 
     public void setParameters(String parameters) {
         this.parameters = parameters;
+    }
+
+    public String getRoute() {
+        return route;
+    }
+
+    public void setRoute(String route) {
+        this.route = route;
+    }
+
+    private HashMap<BaseController, ApiController> getControllers(){
+        // TODO: Use enum for controller classes?
+        HashMap<BaseController, ApiController> annotations = new HashMap<>();
+        annotations.put(new TalksController(), TalksController.class.getAnnotation(ApiController.class));
+        annotations.put(new ScheduleController(), ScheduleController.class.getAnnotation(ApiController.class));
+        return annotations;
+    }
+
+    @Override
+    public Method parseRoute(HttpMethod method, String route) throws Exception {
+        throw new Exception("Not implemented!");
     }
 }
