85e2278f1be865fd0bcab0d739924e94f73bb395
alphonsesantoro <kslagnes@mimer.no>
2018-10-21 17:31:14 +0200

    Query all records + test

--- orm/src/test/java/no/kristiania/pgr200/orm/TestData/UserModel.java
+++ orm/src/test/java/no/kristiania/pgr200/orm/TestData/UserModel.java
@@ -1,10 +1,12 @@
 package no.kristiania.pgr200.orm.TestData;
 
 import no.kristiania.pgr200.orm.BaseRecord;
-import no.kristiania.pgr200.orm.ColumnValue;
-import no.kristiania.pgr200.orm.IBaseModel;
+import no.kristiania.pgr200.orm.Query;
 
-import java.util.Map;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.UUID;
 
 public class UserModel extends BaseRecord<User>{
@@ -25,5 +27,15 @@ public class UserModel extends BaseRecord<User>{
     @Override
     public String getTable() {
         return "users";
+    }
+
+    @Override
+    public List<User> all() throws SQLException {
+        ResultSet results = queryStatement(new Query<User>(this.getTable(), state.getAttributes().keySet().toArray(new String[0])));
+        List<User> list = new LinkedList<>();
+        while (results.next()) {
+            list.add(new User(UUID.fromString(results.getString("id")), results.getString("name"), results.getString("email")));
+        }
+        return list;
     }
 }
