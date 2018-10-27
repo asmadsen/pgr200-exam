4143ec852c02ce5b70358d6a4f58d5f1f3f5eafc
Andreas Storesund Madsen <andreas@asmadsen.no>
2018-10-27 14:16:49 +0200

    ✨ Added Listable as type for when not sure if method returns list or single value

---
+++ orm/src/main/java/no/kristiania/pgr200/orm/Generics/Listable.java
@@ -0,0 +1,40 @@
+package no.kristiania.pgr200.orm.Generics;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+
+public class Listable<T> {
+    private T value = null;
+    private Collection<T> listValue = null;
+    private boolean isList = false;
+
+    public Listable(T value) {
+        this.value = value;
+    }
+
+    public Listable(Collection<T> listValue) {
+        this.listValue = listValue;
+        this.isList = true;
+    }
+
+    public boolean isList() {
+        return this.isList;
+    }
+
+    public T getValue() {
+        if (!this.isList()) {
+            return this.value;
+        }
+        return null;
+    }
+
+    public Collection<T> getListValue() {
+        if (this.value != null) {
+            return Arrays.asList(this.value);
+        } else if (this.listValue != null) {
+            return this.listValue;
+        }
+        return new ArrayList<>();
+    }
+}
